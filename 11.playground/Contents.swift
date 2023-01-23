3d4912fcd7b5a1fa9c036a002a76ec9a728740f6
Raneem AlRashoud <439200695@student.ksu.edu.sa>
2023-01-23 09:28:38 +0300

    11

---
+++ 11.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+// MARK: Part 1️⃣
+
+func add_five(arr: [String]) -> [String] {
+    if arr.isEmpty {
+        return arr }
+    return arr.map{$0 + "5"}
+}
+
+add_five( arr: ["code","c"])
+
+
+// MARK: Part 2️⃣
+func middle_char(word: String) -> String {
+    let wordLength = word.count
+    var returnedChars : String = ""
+    let startIndex : String.Index
+    let endIndex   : String.Index
+    
+    //Case#01 [1 char/empty? return whole word]:
+    if  wordLength <= 1 {
+        return word
+    }
+    
+    //📍New term learned in Swift [offsetBy]
+    //offsetBy: Returns an index that is the specified distance from the given index
+    startIndex = word.index(word.startIndex, offsetBy: (wordLength - 1)/2)
+    endIndex   = word.index(word.startIndex, offsetBy: (wordLength + 2)/2)
+    
+    //Case#02 [<word> has even length]
+    //Thus, middle should be 2 chars
+    if wordLength % 2 == 0 {
+        returnedChars = "" + word[startIndex..<endIndex]
+        return returnedChars
+        
+    }
+    
+    //Case#03 [<word> has odd length]
+    //Thus, middle should be 1 char only
+    returnedChars = "" + word[startIndex...startIndex]
+    return returnedChars
+}
+middle_char(word: "HERE")
+middle_char(word: "HER")
+
+
